e91d17aa80a751ff7af1808a6bf6e412676eab29
zhouleibo <mobai@tsign.cn>
2018-11-12 19:23:52 +0800

    先简单点

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM java:8
+
 ADD target/config-server-0.0.1-SNAPSHOT.jar app.jar
+
 VOLUME /tmp
 VOLUME /target
+
 RUN bash -c 'touch /app.jar'
+
 EXPOSE 8888
+
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/io/ushi/ConfigServerApplication.java
+++ src/main/java/io/ushi/ConfigServerApplication.java
@@ -2,13 +2,15 @@ package io.ushi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args); //NOSONAR
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
